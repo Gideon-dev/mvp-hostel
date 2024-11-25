25713dd036534921ba54193f018bb490276025dc
gideon <adeniranakinloluwa2000@gmail.com>
2024-11-25 01:03:17 +0100

    flexible payment logic

--- js/selectroompage.js
+++ js/selectroompage.js
@@ -39,4 +39,126 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+
+const roomNo = document.querySelectorAll(".r-no-box");
+roomNo.forEach((room) => room.addEventListener("click",()=>{
+  console.log(room.innerHTML);
+  
+  roomNo.forEach((r)=> r.classList.remove("bg-brown"));
+  room.classList.add("bg-brown");
+  
+}));
+
+//to do- to make sure a previously selected unrelevent checkbox isnt issued, 
+// if the user didnt uncheck it and selects another option.
+
+const flexiPlans = document.querySelectorAll('input[data-type="flex-plan"]'); 
+flexiPlans.forEach((plan)=> {
+  plan.addEventListener("click", (e) => {
+
+    //making sure multiple checkboxes aren't selected
+    if(e.target.checked){
+      flexiPlans.forEach((select) => select !== e.target ? select.checked = false : select.checked = true);
+
+
+      
+      const parentContainer = e.target.closest(".sub-option");
+      
+      if(parentContainer){
+        const PriceForSemester = parentContainer.querySelector(".sem-price");
+        const PriceForSession = parentContainer.querySelector(".ses-price");
+        
+        //selecting the appropiate option of the checked flex pay
+        const semOption = parentContainer.querySelector(".sem-option input");
+        const sesOption = parentContainer.querySelector(".ses-option input");
+
+        //get the data price of each input
+        const semPrice = parseFloat(semOption.dataset.price);
+        const sesPrice = parseFloat(sesOption.dataset.price);
+
+        //calculate the 30% flex-payment
+        const flexSem = semPrice * 0.3;
+        const flexSes = sesPrice * 0.3;
+
+        //formatting the estimated price for semester
+        const formattedSemPrice = formatCurrency(flexSem);
+
+        //formatting the estimated price for session
+        const formattedSesPrice = formatCurrency(flexSes);
+
+        //updating the prices
+        PriceForSemester.innerHTML = formattedSemPrice;
+        if(semOption){
+          semOption.setAttribute("data-price", flexSem.toFixed(2))
+        }
+        // console.log(semOption, sesOption);
+        
+
+        PriceForSession.innerHTML = formattedSesPrice;
+        if(sesOption){
+          sesOption.setAttribute("data-price", flexSes.toFixed(2))
+        }
+
+          // Store the original prices for later use
+        PriceForSemester.dataset.originalPrice = semPrice;
+        PriceForSession.dataset.originalPrice = sesPrice;
+
+
+          
+      }
+
+    }else{
+      // Reverting to the original prices
+      const parentContainer = e.target.closest(".sub-option");
+
+      if (parentContainer) {
+        const PriceForSemester = parentContainer.querySelector(".sem-price");
+        const PriceForSession = parentContainer.querySelector(".ses-price");
+
+        const semPrice = parseFloat(PriceForSemester.dataset.originalPrice);
+        const sesPrice = parseFloat(PriceForSession.dataset.originalPrice);
+
+        const semOption = parentContainer.querySelector(".sem-option input");
+        const sesOption = parentContainer.querySelector(".ses-option input");
+
+        // console.log(semOption, sesOption);
+
+
+        // Update the displayed prices to original price
+        PriceForSemester.innerHTML = formatCurrency(semPrice); 
+        semOption.setAttribute("data-price", semPrice.toFixed(2));
+
+        PriceForSession.innerHTML = formatCurrency(sesPrice);
+        sesOption.setAttribute("data-price", sesPrice.toFixed(2));
+
+      }
+    }
+
+  });
+
+});
+
+function formatCurrency(amount) {
+  return new Intl.NumberFormat('en-NG', {
+    style: 'currency',
+    currency: 'NGN',
+  }).format(amount);
+};
+
+
+// document.getElementById("select-room-form").addEventListener("submit",(e) =>{
+//   e.preventDefault();
+//   // const selectedRoomCategory = $("input[name='selected_room_category']:checked").val();
+//   // const rate_type = $("input[name='hostel_rate']:checked").val();
+//   // const rate_price = $("input[name='hostel_rate']:checked").data("price");
+
+//   const selectedRoomCategory = document.querySelector("input[name='selected_room_category']:checked").value;
+//   const rate_type = document.querySelector("input[name='hostel_rate']:checked").value;
+//   const rate_price = document.querySelector("input[name='hostel_rate']:checked").dataset.price;
+
+//   console.log(selectedRoomCategory, rate_type, rate_price);
+  
+// })
+
+
   
